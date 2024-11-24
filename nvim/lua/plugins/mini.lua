bbadf88f394f4fb2e5141367797fc3ac55ba4e38
ayumodark <aayanmohammad@proton.me>
2024-11-24 15:54:03 +0600

    Removed a keybind, q in normal mode has the same functionality

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -8,7 +8,6 @@ return 	{
     require("mini.git").setup()
 
     vim.keymap.set("n", "<leader>fr", ":lua MiniFiles.open()<CR>")
-    vim.keymap.set("n", "<leader>fc", ":lua MiniFiles.close()<CR>")
 
     vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
     vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
