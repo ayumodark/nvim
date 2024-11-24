cec5385fc82bc4d247da2760ecccba3502f77a7a
ayumodark <aayanmohammad@proton.me>
2024-11-24 15:55:03 +0600

    fixed keybinds

--- nvim/lua/plugins/mini.lua
+++ nvim/lua/plugins/mini.lua
@@ -11,6 +11,6 @@ return 	{
 
     vim.keymap.set("n", "<leader>ga", ":Git add .<CR>")
     vim.keymap.set("n", "<leader>gc", ":Git commit<CR>")
-    vim.keymap.set("n", "<leader>gh", ":Git push<CR>")
+    vim.keymap.set("n", "<leader>gp", ":Git push<CR>")
   end,
 }
