3b3da0bd4a3bd468bc0f2011a82b3856536eb7f3
ayumo_dark <aayanmohammad@proton.me>
2024-11-25 20:50:59 +0600

    Update UX.lua
    Added keybindings

--- lua/plugins/UX.lua
+++ lua/plugins/UX.lua
@@ -1,14 +1,17 @@
 return {
   {
     "echasnovski/mini.pick",
+    opts = function()
+      vim.keymap.set("n", "<leader>p", ":Pick files<cr>")
+    end,
   },
   {
     "echasnovski/mini.files",
+    opts = function()
+       vim.keymap.set("n", "<leader>f", ":lua MiniFiles.open()<cr>")
+    end,
   },
   {
     "echasnovski/mini.pairs",
-    opts = function()
-
-    end,
   },
 }
