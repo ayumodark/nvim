35f340ab8f61aa75150d106a1b4a04b7562b19c1
ayumo_dark <aayanmohammad@proton.me>
2024-11-25 12:43:42 +0600

    Create UX.lua

---
+++ lua/plugins/UX.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    "echasnovski/mini.pick",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.files",
+    opts = {},
+  },
+  {
+    "echasnovski/mini.pairs",
+    opts = {},
+  },
+}
