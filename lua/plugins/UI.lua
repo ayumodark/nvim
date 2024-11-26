6bcc29bc4bf49fc5a569c7fb57006cf197721e3c
ayumodark <aayanmohammad@proton.me>
2024-11-26 18:41:14 +0600

    Added treesitter.nvim

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -29,4 +29,25 @@ return {
       }
     end,
   },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    build = ":TSUpdate",
+    config = function()
+      local config = require("nvim-treesitter.configs")
+      config.setup({
+        sync_install = true,
+        auto_install = true,
+        highlight = {
+          enable = true,
+          disable = function(lang, buf)
+            local max_filesize = 100 * 1024 -- 100 KB
+            local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+            if ok and stats and stats.size > max_filesize then
+              return true
+            end
+          end,
+        },
+      })
+    end
+  },
 }
