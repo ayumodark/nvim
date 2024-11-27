45959f2f8b228af74f4fc4b05ebbc3dc471a05c8
ayumo_dark <aayanmohammad@proton.me>
2024-11-27 22:13:08 +0600

    delete duplicate and center cmd

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -16,10 +16,6 @@ return {
     opts = {},
   },
   {
-    "j-hui/fidget.nvim",
-    opts = {},
-  },
-  {
     "folke/noice.nvim",
     dependencies = {
       "MunifTanjim/nui.nvim",
@@ -35,7 +31,7 @@ return {
         },
         presets = {
           bottom_search = true,
-          command_palette = true,
+          command_palette = false,
           long_message_to_split = true,
           inc_rename = false,
           lsp_doc_border = false,
