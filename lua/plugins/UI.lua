da32d31b6ab1bef561b0b573f2fcafc83d43d471
ayumodark <aayanmohammad@proton.me>
2024-11-27 22:06:13 +0600

    better notifications added

--- lua/plugins/UI.lua
+++ lua/plugins/UI.lua
@@ -16,6 +16,34 @@ return {
     opts = {},
   },
   {
+    "j-hui/fidget.nvim",
+    opts = {},
+  },
+  {
+    "folke/noice.nvim",
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+    },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            ["cmp.entry.get_documentation"] = true,
+          },
+        },
+        presets = {
+          bottom_search = true,
+          command_palette = true,
+          long_message_to_split = true,
+          inc_rename = false,
+          lsp_doc_border = false,
+        },
+      })
+    end,
+  },
+  {
     "lukas-reineke/indent-blankline.nvim",
     main = "ibl",
     opts = {},
