cd1ae755d42aab237e5058f6c84671ab911bf72a
ayumo_dark <aayanmohammad@proton.me>
2024-12-01 07:44:25 +0600

    Added markdown-preview

--- lua/plugins/UX.lua
+++ lua/plugins/UX.lua
@@ -23,4 +23,14 @@ return {
     event = "InsertEnter",
     opts = {},
   },
+  {
+    "iamcco/markdown-preview.nvim",
+    lazy = true,
+    cmd = {"MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"},
+    build = "cd app && npm install",
+    init = function()
+      vim.g.mkdp_filetypes = {"markdown"}
+    end,
+    ft = {"markdown"},
+  },
 }
