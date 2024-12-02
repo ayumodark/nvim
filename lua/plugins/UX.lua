9461cc7287f728e90a5047bfc67c44a60c0b9234
ayumodark <aayanmohammad@proton.me>
2024-12-02 15:22:06 +0600

    Not loading markdown at start

--- lua/plugins/UX.lua
+++ lua/plugins/UX.lua
@@ -27,10 +27,6 @@ return {
     "iamcco/markdown-preview.nvim",
     lazy = true,
     cmd = {"MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop"},
-    build = "cd app && npm install",
-    init = function()
-      vim.g.mkdp_filetypes = {"markdown"}
-    end,
-    ft = {"markdown"},
+    opts = {},
   },
 }
