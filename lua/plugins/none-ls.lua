3a68a4bf85ae0eaf435b59c66301dff5f5dbab89
ayumodark <aayanmohammad@proton.me>
2024-12-19 09:00:44 +0600

    added code actions and hover documents

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -1,15 +1,17 @@
 return {
-	"nvimtools/none-ls.nvim",
-	keys = "<leader>r",
-	config = function()
-		local null_ls = require("null-ls")
-		null_ls.setup({
-			sources = {
-				null_ls.builtins.formatting.stylua,
-				null_ls.builtins.formatting.black,
-				null_ls.builtins.formatting.prettierd,
-			},
-		})
-		vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.format()<cr>")
-	end,
+  "nvimtools/none-ls.nvim",
+  keys = "<leader>r",
+  config = function()
+    local null_ls = require("null-ls")
+    null_ls.setup({
+      sources = {
+        null_ls.builtins.formatting.stylua,
+        null_ls.builtins.formatting.black,
+        null_ls.builtins.formatting.prettierd,
+      },
+    })
+    vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.format()<cr>")
+    vim.keymap.set("n", "<leader>ca", "<cmd>lua vim.lsp.buf.code_action()<cr>")
+    vim.keymap.set("n", "<leader>h", "<cmd>lua vim.lsp.buf.hover()<cr>")
+  end,
 }
